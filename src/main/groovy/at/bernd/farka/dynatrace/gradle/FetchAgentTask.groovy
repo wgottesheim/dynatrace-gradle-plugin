8b8cdfb5db2d0cfa8787a2de9005b7957a65c937
Bernd Farka <bernd.farka@gmail.com>
2015-11-28 13:32:43 +0100

    adding travis support

--- src/main/groovy/at/bernd/farka/dynatrace/gradle/FetchAgentTask.groovy
+++ src/main/groovy/at/bernd/farka/dynatrace/gradle/FetchAgentTask.groovy
@@ -1,9 +1,16 @@
 package at.bernd.farka.dynatrace.gradle
 
 import org.gradle.api.DefaultTask
+import org.gradle.internal.os.OperatingSystem
 
 /**
  * Created by bernd on 28.11.2015.
  */
 class FetchAgentTask extends DefaultTask {
+
+
+    public void download(){
+        OperatingSystem.current()
+    }
+
 }
